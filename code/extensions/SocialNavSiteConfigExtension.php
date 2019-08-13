f7c2819a9c2a968127d4809fc1e692aaa75da1b4
Hadlee Lineham <intotheweb101@gmail.com>
2019-08-13 12:36:52 +1200

    Adding in missing name spacing

--- code/extensions/SocialNavSiteConfigExtension.php
+++ code/extensions/SocialNavSiteConfigExtension.php
@@ -1,5 +1,11 @@
 <?php
 
+use SilverStripe\Forms\FieldList;
+use SilverStripe\ORM\DataExtension;
+use SilverStripe\Forms\GridField\GridField;
+use SilverStripe\Forms\ToggleCompositeField;
+use SilverStripe\Forms\GridField\GridFieldConfig_RecordEditor;
+
 /**
  * Add additional variables to default siteconfig
  *
@@ -10,28 +16,28 @@ class SocialNavSiteConfigExtension extends DataExtension
 {
 
     public static $has_many = array(
-        'SocialNavLinks' => 'SocialNavLink'
+        'SocialNavLinks' => SocialNavLink::class
     );
 
     public function updateCMSFields(FieldList $fields)
     {
-        $fields->removeByName('SocialNavLinks');
+        // $fields->removeByName('SocialNavLinks');
 
-        // Setup compressed postage options
-        $socialnav_fields = ToggleCompositeField::create(
-            'SocialNavFields',
-            'Social Nav',
-            array(
-                GridField::create(
-                    'SocialNavLinks',
-                    '',
-                    $this->owner->SocialNavLinks(),
-                    GridFieldConfig_RecordEditor::create()
-                )
-            )
-        );
+        // // Setup compressed postage options
+        // $socialnav_fields = ToggleCompositeField::create(
+        //     'SocialNavFields',
+        //     'Social Nav',
+        //     array(
+                
+        //     )
+        // );
 
         // Add config sets
-        $fields->addFieldToTab('Root.Main', $socialnav_fields);
+        $fields->addFieldToTab('Root.Main', GridField::create(
+            'SocialNavLinks',
+            '',
+            $this->owner->SocialNavLinks(),
+            GridFieldConfig_RecordEditor::create()
+        ));
     }
 }
